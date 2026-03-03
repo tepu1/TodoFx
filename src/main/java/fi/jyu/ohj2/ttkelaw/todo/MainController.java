e7eb22402dfb7d65be9d1147a7e07678b2d80a8f
Tero Kela <tero.t.kela@gmail.com>
2026-03-03 16:35:19 +0200

    Tehtävien lukeminen ja tallentaminen tehty

--- src/main/java/fi/jyu/ohj2/ttkelaw/todo/MainController.java
+++ src/main/java/fi/jyu/ohj2/ttkelaw/todo/MainController.java
@@ -8,11 +8,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import  javafx.scene.control.CheckBox;
+import tools.jackson.core.JacksonException;
+import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
 
 import java.awt.*;
 import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -30,9 +34,67 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        lataa();
         lisaaUusiTehtavaPainike.setOnAction(event -> lisaaTehtava());
         uusiTehtavaNimi.setOnAction(event -> lisaaTehtava());
     }
+
+
+
+    private List<Tehtava> haeTehtavat(VBox sailio) {
+        return sailio.getChildren().stream()
+                .map(n -> (CheckBox) n)
+                .map(cb -> new Tehtava(cb.getText(), cb.isSelected()))
+                .toList();
+    }
+    private void tallenna() {
+        List<Tehtava> tehtavat = new ArrayList<>();
+        tehtavat.addAll(haeTehtavat(tekemattomat));
+        tehtavat.addAll(haeTehtavat(tehdyt));
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(Path.of("tehtavat.json"), tehtavat);
+    }
+    private void lataa() {
+        Path path = Path.of("tehtavat.json");
+        if (Files.notExists(path)) {
+            return;
+        }
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            List<Tehtava> tehtavat = mapper.readValue(path, new TypeReference<>() {});
+            for (Tehtava t : tehtavat) {
+                CheckBox cb = luoCheckBox(t.getTeksti(), t.isTehty());
+                if (t.isTehty()) {
+                    tehdyt.getChildren().add(cb);
+                }
+                else {
+                    tekemattomat.getChildren().add(cb);
+                }
+            }
+
+        } catch (JacksonException e) {
+            IO.println("Virhe: " + e.getMessage());
+        }
+    }
+
+
+
+    private CheckBox luoCheckBox(String teksti, boolean tehty) {
+        CheckBox tehtava = new CheckBox(teksti);
+        tehtava.setSelected(tehty);
+        tehtava.setOnAction(event -> {
+            if (tehtava.isSelected()) {
+                tekemattomat.getChildren().remove(tehtava);
+                tehdyt.getChildren().add(tehtava);
+            } else {
+                tehdyt.getChildren().remove(tehtava);
+                tekemattomat.getChildren().add(tehtava);
+            }
+            tallenna();
+        });
+        return tehtava;
+    }
+
 
     private void lisaaTehtava() {
         String teksti = uusiTehtavaNimi.getText();
@@ -40,27 +102,10 @@ public class MainController implements Initializable {
             uusiTehtavaNimi.requestFocus();
             return;
         }
-        teksti = teksti.trim();
-        CheckBox tehtava = new CheckBox(teksti);
-        tehtava.setOnAction(event -> {
-           if (tehtava.isSelected()) {
-               tekemattomat.getChildren().remove(tehtava);
-               tehdyt.getChildren().add(tehtava);
-           } else {
-               tehdyt.getChildren().remove(tehtava);
-               tekemattomat.getChildren().add(tehtava);
-           }
-        });
-        tekemattomat.getChildren().add(tehtava);
+        tekemattomat.getChildren().add(luoCheckBox(teksti, false));
         uusiTehtavaNimi.clear();
         uusiTehtavaNimi.requestFocus();
-        List<Tehtava> tekemattomatList = tekemattomat.getChildren().stream()
-                .map(n -> (CheckBox) n)
-                .map(cb -> new Tehtava(cb.getText(), cb.isSelected()))
-                .toList();
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(Path.of("tehtavat.json"), tekemattomatList);
-
+        tallenna();
 
     }
 }
